dbca6ebbe473c72e395cd46d61b63acab452c639
Yassin Bahassane <yassinfreethinker@gmail.com>
2019-10-06 22:07:26 +0200

    Add functionalities

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {MOVIES} from './shared/movies';
 import './App.css';
 
 const RenderMovies = ({movies}) => {
+  if(movies.length === 0)
+    return <p className="noresult">No result is found!</p>
   return (
      movies.map( movie => {
        return (<div key={movie.id} className="movie">
@@ -16,20 +18,32 @@ const RenderMovies = ({movies}) => {
 class App extends Component {
 
    state = {
-      movies: MOVIES
+      movies: MOVIES,
+      search: ''
+   }
+
+   onChange = (e) => {      
+      this.setState({search: e.target.value});
+   }
+
+   filteredMovies = (movies) => {
+      const {search} = this.state;
+      return movies.filter(movie => movie.name.toLocaleLowerCase()
+      .search(search.toLocaleLowerCase()) !== -1);
    }
 
   render(){
+    //console.log(this.state.search);
     const {movies} = this.state;
     return (
       <div className="App">
         <h1>My best movies</h1>
         <div className="search">
-          <input type="text" name="search" value=""/>
+          <input type="text" name="search" onChange={this.onChange} value={this.state.search}/>
           <button type="submit">search</button>
         </div>
         <div className="grid">
-          <RenderMovies movies={movies} />
+          <RenderMovies movies={this.filteredMovies(movies)} />
         </div>
       </div>
     );
