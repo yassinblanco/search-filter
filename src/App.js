5486de045105d34d6131f0bd2fb2c481611f57db
Yassin Bahassane <yassinfreethinker@gmail.com>
2019-10-06 18:47:29 +0200

    Add search field and grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {MOVIES} from './shared/movies';
 import './App.css';
 
-function App() {
+const RenderMovies = ({movies}) => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+     movies.map( movie => {
+       return (<div key={movie.id} className="movie">
+                <img src={movie.image} alt={movie.name} />
+                <h3>{movie.name}</h3>
+              </div>);
+     })
   );
+}
+
+class App extends Component {
+
+   state = {
+      movies: MOVIES
+   }
+
+  render(){
+    const {movies} = this.state;
+    return (
+      <div className="App">
+        <h1>My best movies</h1>
+        <div className="search">
+          <input type="text" name="search" value=""/>
+          <button type="submit">search</button>
+        </div>
+        <div className="grid">
+          <RenderMovies movies={movies} />
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
