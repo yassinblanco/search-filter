e61f00994efebc99bb39cf0ec249c3e6dc419c76
Yassin Bahassane <yassinfreethinker@gmail.com>
2019-10-06 18:00:39 +0200

    Initial Setup

---
+++ src/shared/movies.js
@@ -0,0 +1,94 @@
+export const MOVIES = [
+    {
+      id: 1,
+      name: "Seven Samurai",
+      country: "Japan",
+      director: "Akira Kurosawa",
+      image: "assets/images/seven_samurai.jpg",
+      genre: "Adventure",
+      year: 1954,
+      rate: 8.6
+    },
+    {
+      id: 2,
+      name: "Oldboy",
+      country: "Korea",
+      director: "Park Chan-wook",
+      image: "assets/images/oldboy.jpg",
+      genre: "Drama",
+      year: 2003,
+      rate: 8.4
+    },
+    {
+      id: 3,
+      name: "The Secret in Their Eyes",
+      country: "Argentina",
+      director: "Juan José Campanella",
+      image: "assets/images/the_secret_in_their_eyes.jpg",
+      year: 2009,
+      rate: 8.2
+    },
+    {
+      id: 4,
+      name: "Parasite",
+      country: "Korea",
+      director: "Bong Joon Ho",
+      image: "assets/images/parasite.jpg",
+      year: 2019,
+      rate: 8.5
+    },
+    {
+      id: 5,
+      name: "Rashomon",
+      country: "Japan",
+      director: "Akira Kurosawa",
+      image: "assets/images/rashomon.jpg",
+      year: 1950,
+      rate:8.2
+    },
+    {
+      id: 6,
+      name: "Wild Tales",
+      country: "Argentina",
+      director: "Damián Szifron",
+      image: "assets/images/wild_tales.jpg",
+      year: 2014,
+      rate:8.1
+    },
+    {
+      id: 7,
+      name: "Ikiru",
+      country: "Japan",
+      director: "Akira Kurosawa",
+      image: "assets/images/ikiru.jpg",
+      year: 1952,
+      rate:8.3
+    },
+    {
+      id: 8,
+      name: "Three Idiots",
+      country: "India",
+      director: "Rajkumar Hirani",
+      image: "assets/images/three_idiots.jpg",
+      year: 2009,
+      rate: 8.4
+    },
+    {
+      id: 9,
+      name: "Nine Queens",
+      country: "Argentina",
+      director: "Fabián Bielinsky",
+      image: "assets/images/nine_queens.jpg",
+      year:2000,
+      rate:7.9
+    },
+    {
+      id: 10,
+      name: "A Prophet",
+      country: "France",
+      director: "Jacques Audiard",
+      image: "assets/images/a_prophet.jpg",
+      year: 2009,
+      rate:7.9
+    }
+  ];
