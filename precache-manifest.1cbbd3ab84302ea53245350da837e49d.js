5a7dfcdb0cb072f9ebade9f6050b81163c6a3276
Yassin Bahassane <yassinfreethinker@gmail.com>
2019-10-07 10:54:20 +0200

    Updates

---
+++ precache-manifest.1cbbd3ab84302ea53245350da837e49d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9042596fd2b77bb5c10a5a2d55bdfc",
+    "url": "/search-filter/index.html"
+  },
+  {
+    "revision": "e14e7621d5805963b9be",
+    "url": "/search-filter/static/css/main.94c44a30.chunk.css"
+  },
+  {
+    "revision": "70abd462c13a1b9cf11b",
+    "url": "/search-filter/static/js/2.5f9c6914.chunk.js"
+  },
+  {
+    "revision": "e14e7621d5805963b9be",
+    "url": "/search-filter/static/js/main.456dc173.chunk.js"
+  },
+  {
+    "revision": "5b003f3929edba1e036a",
+    "url": "/search-filter/static/js/runtime-main.e42022f7.js"
+  }
+]);
